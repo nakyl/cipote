239888005a3baf93968298bb3c066c36d74e5245
Usuario <Usuario@DESKTOP-G9QEMI4>
2018-02-01 18:50:51 +0100

    Test

--- src/main/resources/db/migration/V3__INSERT_EXCHANGES.sql
+++ src/main/resources/db/migration/V3__INSERT_EXCHANGES.sql
@@ -1,9 +1,9 @@
-INSERT INTO EXCHANGE (NAME) VALUES('GDAX');
-INSERT INTO EXCHANGE (NAME) VALUES('BINANCE');
-INSERT INTO EXCHANGE (NAME) VALUES('BITTREX');
+INSERT INTO exchange (NAME) VALUES('GDAX');
+INSERT INTO exchange (NAME) VALUES('BINANCE');
+INSERT INTO exchange (NAME) VALUES('BITTREX');
 
 
-INSERT INTO EXCHANGE_API (ID_EXCHANGE, API_URL) VALUES((SELECT ID FROM EXCHANGE WHERE NAME = 'GDAX'), 'https://api.gdax.com/products/{0}-{1}/ticker');
-INSERT INTO EXCHANGE_API (ID_EXCHANGE, API_URL) VALUES((SELECT ID FROM EXCHANGE WHERE NAME = 'BINANCE'), 'https://api.binance.com/api/v1/ticker/price?symbol={0}{1}');
-INSERT INTO EXCHANGE_API (ID_EXCHANGE, API_URL) VALUES((SELECT ID FROM EXCHANGE WHERE NAME = 'BITTREX'), 'https://bittrex.com/api/v1.1/public/getticker?market={1}-{0}');
+INSERT INTO exchange_api (ID_EXCHANGE, API_URL) VALUES((SELECT ID FROM exchange WHERE NAME = 'GDAX'), 'https://api.gdax.com/products/{0}-{1}/ticker');
+INSERT INTO exchange_api (ID_EXCHANGE, API_URL) VALUES((SELECT ID FROM exchange WHERE NAME = 'BINANCE'), 'https://api.binance.com/api/v1/ticker/price?symbol={0}{1}');
+INSERT INTO exchange_api (ID_EXCHANGE, API_URL) VALUES((SELECT ID FROM exchange WHERE NAME = 'BITTREX'), 'https://bittrex.com/api/v1.1/public/getticker?market={1}-{0}');
 
